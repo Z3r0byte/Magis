0a72219faa85cbaa0cf33f78adb40cc708d20a71
Z3r0byte <z3r0byte.apps@gmail.com>
2016-05-14 11:31:02 +0200

    added log tag

--- app/src/main/java/com/z3r0byte/magis/startActivity.java
+++ app/src/main/java/com/z3r0byte/magis/startActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class startActivity extends AppCompatActivity {
 
+    private static final String TAG = "startActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
