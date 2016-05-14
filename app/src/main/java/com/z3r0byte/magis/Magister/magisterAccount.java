a7424e84885233cff5313225e7a4482b19dd1832
Z3r0byte <z3r0byte.apps@gmail.com>
2016-05-14 17:23:05 +0200

    added copyright created magisterAccount class

--- app/src/main/java/com/z3r0byte/magis/Magister/magisterAccount.java
+++ app/src/main/java/com/z3r0byte/magis/Magister/magisterAccount.java
@@ -14,18 +14,8 @@
  *    limitations under the License.
  */
 
-package com.z3r0byte.magis;
+package com.z3r0byte.magis.Magister;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class startActivity extends AppCompatActivity {
-
-    private static final String TAG = "startActivity";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-    }
+public class magisterAccount {
 }
