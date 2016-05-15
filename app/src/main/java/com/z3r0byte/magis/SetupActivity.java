1194f046fb876f11a33e2c8727ab70f622299450
Z3r0byte <z3r0byte.apps@gmail.com>
2016-05-15 11:08:28 +0200

    added icons, started working on Setup

--- app/src/main/java/com/z3r0byte/magis/SetupActivity.java
+++ app/src/main/java/com/z3r0byte/magis/SetupActivity.java
@@ -16,16 +16,59 @@
 
 package com.z3r0byte.magis;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.app.OnNavigationBlockedListener;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+import com.heinrichreimersoftware.materialintro.slide.Slide;
+
+import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
 
 public class SetupActivity extends IntroActivity {
+
+    private static final String TAG = "SetupActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup);
+        CustomActivityOnCrash.install(this);
+
+        setSkipEnabled(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.setup_title_1)
+                .description(R.string.setup_desc_1)
+                .image(R.drawable.magis512)
+                .background(R.color.setup_color_1)
+                .backgroundDark(R.color.setup_color_1)
+                .build());
+
+        final Slide permissionsSlide;
+        permissionsSlide = new SimpleSlide.Builder()
+                .title(R.string.setup_title_2)
+                .description(R.string.setup_desc_2)
+                .image(R.drawable.sdcard)
+                .background(R.color.setup_color_2)
+                .backgroundDark(R.color.setup_color_2)
+                .permission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .build();
+        addSlide(permissionsSlide);
+
+        addOnNavigationBlockedListener(new OnNavigationBlockedListener() {
+            @Override
+            public void onNavigationBlocked(int position, int direction) {
+                View contentView = findViewById(android.R.id.content);
+                Slide slide = getSlide(position);
+
+                if (slide == permissionsSlide) {
+                    Snackbar.make(contentView, R.string.snackbar_no_permissions, Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
 
 
     }
