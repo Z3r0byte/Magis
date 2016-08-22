758d21e6ba4c4aa5f48e4a1796106cb4b105fde2
Z3r0byte <z3r0byte.apps@gmail.com>
2016-08-22 19:47:34 +0200

    Created Service for appointment notifications

--- app/src/main/java/com/z3r0byte/magis/Utils/DB_Handlers/CalendarDB.java
+++ app/src/main/java/com/z3r0byte/magis/Utils/DB_Handlers/CalendarDB.java
@@ -249,6 +249,50 @@ public class CalendarDB extends SQLiteOpenHelper {
         return results;
     }
 
+    public Appointment[] getNotificationAppointments() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Date now = DateUtils.getToday();
+        Date future = DateUtils.addMinutes(now, 25);
+
+        Integer dateInt = Integer.parseInt(DateUtils.formatDate(future, "yyyyMMdd")); //Adding 0's for ability to filter results.
+        String Query = "SELECT * FROM " + TABLE_CALENDAR + " WHERE " + KEY_FORMATTED_START + " <= " + dateInt + " AND "
+                + KEY_FORMATTED_END + " >= " + dateInt;
+        Cursor cursor = db.rawQuery(Query, null);
+
+        Appointment[] results = new Appointment[cursor.getCount()];
+        int i = 0;
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    Gson gson = new Gson();
+                    Appointment appointment = new Appointment();
+                    appointment.id = cursor.getInt(cursor.getColumnIndex(KEY_CALENDAR_ID));
+                    appointment.startDate = DateUtils.parseDate(cursor.getString(cursor.getColumnIndex(KEY_START)), "yyyy-MM-dd'T'HH:mm:ss.0000000'Z'");
+                    appointment.classrooms = gson.fromJson(cursor.getString(cursor.getColumnIndex(KEY_CLASS_ROOMS)), Classroom[].class);
+                    appointment.content = cursor.getString(cursor.getColumnIndex(KEY_CONTENT));
+                    appointment.endDate = DateUtils.parseDate(cursor.getString(cursor.getColumnIndex(KEY_END)), "yyyy-MM-dd'T'HH:mm:ss.0000000'Z'");
+                    appointment.finished = cursor.getInt(cursor.getColumnIndex(KEY_FINISHED)) > 0;
+                    appointment.infoType = InfoType.getTypeById(cursor.getInt(cursor.getColumnIndex(KEY_INFO_TYPE)));
+                    appointment.links = gson.fromJson(cursor.getString(cursor.getColumnIndex(KEY_LINKS)), Link[].class);
+                    appointment.location = cursor.getString(cursor.getColumnIndex(KEY_LOCATION));
+                    appointment.periodFrom = cursor.getInt(cursor.getColumnIndex(KEY_PERIOD_FROM));
+                    appointment.periodUpToAndIncluding = cursor.getInt(cursor.getColumnIndex(KEY_PERIOD_TO));
+                    appointment.description = cursor.getString(cursor.getColumnIndex(KEY_DESC));
+                    appointment.classState = cursor.getInt(cursor.getColumnIndex(KEY_STATE));
+                    appointment.subjects = gson.fromJson(cursor.getString(cursor.getColumnIndex(KEY_SUBJECTS)), SubSubject[].class);
+                    appointment.teachers = gson.fromJson(cursor.getString(cursor.getColumnIndex(KEY_TEACHER)), Teacher[].class);
+                    appointment.type = AppointmentType.getTypeById(cursor.getInt(cursor.getColumnIndex(KEY_TYPE)));
+
+                    results[i] = appointment;
+                    i++;
+                } while (cursor.moveToNext());
+            }
+        }
+        cursor.close();
+
+        return results;
+    }
+
     public void deleteAppointment(Appointment appointment) {
         deleteAppointment(appointment.id);
     }
