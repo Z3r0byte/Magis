d5c89731c56518ecaf314a5064f7f53951e2827b
Z3r0byte <z3r0byte.apps@gmail.com>
2016-09-26 19:01:13 +0200

    Updated version

--- app/src/main/java/com/z3r0byte/magis/Utils/ApiKeyUtil.java
+++ app/src/main/java/com/z3r0byte/magis/Utils/ApiKeyUtil.java
@@ -22,7 +22,7 @@ package com.z3r0byte.magis.Utils;
 
 public class ApiKeyUtil {
 
-    private static final String API_KEY = "EF15";
+    private static final String API_KEY = "API_KEY";
 
     public static String getKey() {
         return API_KEY;
