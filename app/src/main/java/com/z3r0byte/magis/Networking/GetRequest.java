181c248e81701fd1d5bebceff1ad2334c623e939
Z3r0byte <z3r0byte.apps@gmail.com>
2016-10-05 19:27:17 +0200

    Implemented status item in Navigation drawer, bug fixes, RELEASE 1.2.3 (BETA)

--- app/src/main/java/com/z3r0byte/magis/Networking/GetRequest.java
+++ app/src/main/java/com/z3r0byte/magis/Networking/GetRequest.java
@@ -19,14 +19,10 @@ package com.z3r0byte.magis.Networking;
 import android.util.Log;
 
 import java.io.IOException;
-import java.util.Collections;
 
-import okhttp3.CipherSuite;
-import okhttp3.ConnectionSpec;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.TlsVersion;
 
 /**
  * Created by basva on 14-5-2016.
@@ -38,16 +34,7 @@ public class GetRequest {
     public static String getRequest(String url, String cookie) throws IOException {
         Log.d(TAG, "getRequest() called with: " + "url = [" + url + "], cookie = [" + cookie + "]");
 
-        ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                .tlsVersions(TlsVersion.TLS_1_2)
-                .cipherSuites(
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
-                .build();
-
         OkHttpClient client = new OkHttpClient.Builder()
-                .connectionSpecs(Collections.singletonList(spec))
                 .build();
 
         Request request;
